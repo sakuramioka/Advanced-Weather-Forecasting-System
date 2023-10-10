841746c70fdd4d79246cdf1e9ba8535021c7c5b7
sakuramioka <lnaresh2006@gmail.com>
2023-10-10 20:42:18 +0530

    Bug fixes

--- UI.py
+++ UI.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import PhotoImage
 from PIL import Image, ImageTk
 import GeocodingAPI
+import ForecastAPI
 
 # Create the main window
 root = tk.Tk()
@@ -21,11 +22,29 @@ canvas.pack()
 # Set the background image on the canvas
 canvas.create_image(0, 0, anchor=tk.NW, image=background_photo)
 
+def display_results(latitude, longitude):
+    info_text = canvas.create_text(root.winfo_screenwidth/2, root.winfo_screenheight/2, anchor=tk.NW, text="Please wait... fetching information!",
+                                   font=('Century Gothic', '15', 'bold'))
+    data = ForecastAPI.get_forecast(latitude,longitude,'temperature_2m','auto')
+
+def callback(event):
+    curtext = entry.get()
+    selection = event.widget.curselection()
+    if selection:
+        index = selection[0]
+        data = event.widget.get(index)
+        entry.delete(0, tk.END)
+        entry.insert(0, data)
+    else:
+        entry.delete(0, tk.END)
+        entry.insert(0, curtext)
+
 result_list = None
 def search_results():
     global result_list
     keywd = entry.get()  
     try:
+        canvas.delete('info_text')
         result_list.destroy()
     except Exception:
         pass
@@ -34,7 +53,14 @@ def search_results():
     var = tk.Variable(value=list_items)
     result_list = tk.Listbox(canvas, listvariable=var, justify='center', font=('Century Gothic', '15'), selectbackground='#87ceeb', selectforeground='#000000', highlightthickness=0)
     result_list.place(x=(root.winfo_screenwidth() - 600) // 2 - 30, y=360, width=600, height=len(list_items*24))
-    canvas.create_text((root.winfo_screenwidth())// 2 - 30, 365+len(list_items*24), font=('Century Gothic', '15', 'bold'), text=f"Your query produced {len(list_items)} results!", anchor=tk.NE)
+    result_list.bind("<<ListboxSelect>>", callback)
+    canvas.create_text((root.winfo_screenwidth())// 2 - 30, 365+len(list_items*24), font=('Century Gothic', '15', 'bold'), text=f"Your query produced {len(list_items)} results!", anchor=tk.NE
+                       ,tags='info_text')
+
+# Function to clear the default text when clicked
+def clear_default_text(event):
+    if entry.get() == "Enter a city name":
+        entry.delete(0, tk.END)
 
 x_spacing = 70
 y_spacing = 5
@@ -61,11 +87,6 @@ entry.insert(0, "Enter a city name")
 entry.place(x=(root.winfo_screenwidth() - 600) // 2 - 30, y=300, width=600, height=60)
 search_button = tk.Button(canvas, text="🔎", width=4, height=1, font=('Century Gothic', 20), relief='flat', command=search_results)
 search_button.place(x=(root.winfo_screenwidth() - 600) // 2 + 570, y=300, width=60, height=60)
-
-# Function to clear the default text when clicked
-def clear_default_text(event):
-    if entry.get() == "Enter a city name":
-        entry.delete(0, tk.END)
 
 # Bind the click event to clear the default text
 entry.bind("<Button-1>", clear_default_text)
